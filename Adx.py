c5cb6a322260350c71e2565e3d9cd1ffca6abe28
hermannKonyar <er.konyar@gmail.com>
2023-05-16 12:15:24 +0300

    Fifth Commit

--- Adx.py
+++ Adx.py
@@ -7,14 +7,24 @@ import numpy as np
 import talib
 
 app = Flask(__name__)
-cache = Cache(app, config={'CACHE_TYPE': 'simple'})
+
+# Configuration for the first cache set (simple cache type)
+app.config['CACHE_TYPE'] = 'simple'
+app.config['CACHE_DEFAULT_TIMEOUT'] = 3600
+cache_simple = Cache(app)
+
+# Configuration for the second cache set (memcached cache type)
+app.config['CACHE_TYPE'] = 'memcached'
+app.config['CACHE_MEMCACHED_SERVERS'] = ['127.0.0.1:11211']
+app.config['CACHE_DEFAULT_TIMEOUT'] = 3600
+cache_memcached = Cache(app)
 
 @app.route('/macd')
 def get_macd_data():
     symbols = ["ETHUSDT", "BTCUSDT", "BNBUSDT", "RSRUSDT", "LUNAUSDT", "BUSDUSDT", "SHIBUSDT", "CHZUSDT", "ADAUSDT", "SUNUSDT", "LITUSDT", "ATOMUSDT", "XRPUSDT"]
     intervals = ['15m', '30m', '1h', '4h', '1d']
     limit = 101
-    result = {}
+    result = {}  # Define an empty dictionary
 
     for symbol in symbols:
         for interval in intervals:
@@ -32,9 +42,9 @@ def get_macd_data():
             previous_macd, previous_macdsignal = macd[-2], macdsignal[-2]
             current_macd, current_macdsignal = macd[-1], macdsignal[-1]
 
-            if previous_macd < previous_macdsignal and current_macd > current_macdsignal and current_macd<0 and previous_macd<0:
+            if previous_macd < previous_macdsignal and current_macd > current_macdsignal:
                 signal = 'BUY'
-            elif previous_macd > previous_macdsignal and current_macd < current_macdsignal and current_macd>0 and previous_macd>0:
+            elif previous_macd > previous_macdsignal and current_macd < current_macdsignal:
                 signal = 'SELL'
             else:
                 signal = ''
@@ -53,18 +63,25 @@ def get_macd_data():
                     'interval': interval
                 })
 
-    cache.set('macd_data', result, timeout=3600)
+    cache_simple.set('macd_data', result, timeout=3600)
+    cache_memcached.set('macd_data', result, timeout=3600)
     return jsonify(result)
 
 @app.route('/macd/<string:interval>')
 def get_macd_interval(interval):
-    data = cache.get('macd_data')
-    if data is None:
+    data_simple = cache_simple.get('macd_data')
+    data_memcached = cache_memcached.get('macd_data')
+
+    if data_simple is None or data_memcached is None:
         return jsonify({'error': 'MACD data not available'})
 
-    result = {symbol: [entry for entry in data[symbol] if entry['interval'] == interval] for symbol in data}
-    cache.set('macd_data_interval', result, timeout=3600)
-    return jsonify(result)
+    result_simple = {symbol: [entry for entry in data_simple[symbol] if entry['interval'] == interval] for symbol in data_simple}
+    result_memcached = {symbol: [entry for entry in data_memcached[symbol] if entry['interval'] == interval] for symbol in data_memcached}
+
+    cache_simple.set('macd_interval_data', result_simple, timeout=3600)
+    cache_memcached.set('macd_interval_data', result_memcached, timeout=3600)
+
+    return jsonify({'result_simple': result_simple, 'result_memcached': result_memcached})
 
 
 if __name__ == '__main__':
