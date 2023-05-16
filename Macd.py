02d150c7b928e942ec5396788c5353cb28268a89
hermannKonyar <er.konyar@gmail.com>
2023-05-16 09:53:53 +0300

    Fourth Commit

--- Macd.py
+++ Macd.py
@@ -1,34 +1,34 @@
 from flask import Flask, jsonify
 from flask_caching import Cache
+import time
 import requests
 import json
 import numpy as np
 import talib
-import time
 
 app = Flask(__name__)
 cache = Cache(app, config={'CACHE_TYPE': 'simple'})
 
-symbols = ["ETHUSDT", "BTCUSDT", "BNBUSDT", "RSRUSDT", "LUNAUSDT", "BUSDUSDT", "SHIBUSDT", "CHZUSDT", "ADAUSDT", "SUNUSDT", "LITUSDT", "ATOMUSDT", "XRPUSDT",]
-intervals = [ '15m', '30m',  '1h',  '4h', '1d']
-limit = 101
-
-@cache.cached(timeout=3600, key_prefix='macd_data')
+@app.route('/macd')
 def get_macd_data():
+    symbols = ["ETHUSDT", "BTCUSDT", "BNBUSDT", "RSRUSDT", "LUNAUSDT", "BUSDUSDT", "SHIBUSDT", "CHZUSDT", "ADAUSDT", "SUNUSDT", "LITUSDT", "ATOMUSDT", "XRPUSDT"]
+    intervals = ['15m', '30m', '1h', '4h', '1d']
+    limit = 101
     result = {}
+
     for symbol in symbols:
-        result[symbol] = {}
         for interval in intervals:
             response = requests.get(f"https://api.binance.com/api/v3/klines?symbol={symbol}&interval={interval}&limit={limit}")
-            data = json.loads(response.text)
+            if response.status_code != 200:
+                continue
+
+            data = response.json()
 
             close_prices = [float(item[4]) for item in data]
             close_prices_np = np.array(close_prices)
 
-            # Calculate MACD and MACD Signal
             macd, macdsignal, _ = talib.MACD(close_prices_np)
 
-            # Generate signals
             previous_macd, previous_macdsignal = macd[-2], macdsignal[-2]
             current_macd, current_macdsignal = macd[-1], macdsignal[-1]
 
@@ -41,48 +41,30 @@ def get_macd_data():
 
             timestamp = int(time.time() * 1000)
 
-            # Only add to result if there is a cross signal
             if signal:
-                result[symbol][interval] = {
+                if symbol not in result:
+                    result[symbol] = []
+                result[symbol].append({
                     'coin': symbol,
-                    'timeframe': interval,
                     'cross': signal,
                     'macd': current_macd,
                     'macdsignal': current_macdsignal,
-                    'date': timestamp
-                }
+                    'date': timestamp,
+                    'interval': interval
+                })
 
-    return result
+    cache.set('macd_data', result, timeout=3600)
+    return jsonify(result)
 
-@app.route('/macd')
-def getMacd():
-    data = get_macd_data()
-    return jsonify({symbol : data[symbol] for symbol in data})
+@app.route('/macd/<string:interval>')
+def get_macd_interval(interval):
+    data = cache.get('macd_data')
+    if data is None:
+        return jsonify({'error': 'MACD data not available'})
 
-@app.route('/macd15m')
-def getMacd15m():
-    data = get_macd_data()
-    return jsonify({symbol: data[symbol]['15m'] for symbol in data if '15m' in data[symbol]})
+    result = {symbol: [entry for entry in data[symbol] if entry['interval'] == interval] for symbol in data}
+    return jsonify(result)
 
-@app.route('/macd30m')
-def getMacd30m():
-    data = get_macd_data()
-    return jsonify({symbol: data[symbol]['30m'] for symbol in data if '30m' in data[symbol]})
-
-@app.route('/macd1h')
-def getMacd1h():
-    data = get_macd_data()
-    return jsonify({symbol: data[symbol]['1h'] for symbol in data if '1h' in data[symbol]})
-
-@app.route('/macd4h')
-def getMacd4h():
-    data = get_macd_data()
-    return jsonify({symbol: data[symbol]['4h'] for symbol in data if '4h' in data[symbol]})
-
-@app.route('/macd1d')
-def getMacd1d():
-    data = get_macd_data()
-    return jsonify({symbol: data[symbol]['1d'] for symbol in data if '1d' in data[symbol]})
 
 if __name__ == '__main__':
     app.run(debug=True)
