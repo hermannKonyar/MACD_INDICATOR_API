064512c350aa947bb999eafcbd939fe18977c217
hermannKonyar <er.konyar@gmail.com>
2023-05-16 14:13:26 +0300

    seventh commit

--- stotastik.py
+++ stotastik.py
@@ -2,18 +2,18 @@ from flask import Flask, jsonify
 from flask_caching import Cache
 import time
 import requests
+import json
 import numpy as np
 import talib
 
 app = Flask(__name__)
 cache = Cache(app, config={'CACHE_TYPE': 'simple'})
 
-@app.route('/adx')
-def get_adx_data():
+@app.route('/stoch_rsi')
+def get_stoch_rsi_data():
     symbols = ["ETHUSDT", "BTCUSDT", "BNBUSDT", "RSRUSDT", "LUNAUSDT", "BUSDUSDT", "SHIBUSDT", "CHZUSDT", "ADAUSDT", "SUNUSDT", "LITUSDT", "ATOMUSDT", "XRPUSDT"]
     intervals = ['15m', '30m', '1h', '4h', '1d']
-    limit = 31
-
+    limit = 101
     result = {}
 
     for symbol in symbols:
@@ -24,40 +24,49 @@ def get_adx_data():
 
             data = response.json()
 
-            high_prices = [float(item[2]) for item in data]
-            low_prices = [float(item[3]) for item in data]
             close_prices = [float(item[4]) for item in data]
-
-            high_prices_np = np.array(high_prices)
-            low_prices_np = np.array(low_prices)
             close_prices_np = np.array(close_prices)
 
-            adx = talib.ADX(high_prices_np, low_prices_np, close_prices_np)
+            # Calculate RSI
+            rsi = talib.RSI(close_prices_np)
 
-            current_adx = adx[-1]
+            # Calculate Stochastic RSI
+            fastk, fastd = talib.STOCHRSI(rsi)
+            current_fastk, current_fastd = fastk[-1], fastd[-1]
+
+            # Generate signal
+            signal = ''
+            if current_fastk < 30:
+                signal = 'BUY'
+            elif current_fastk > 70:
+                signal = 'SELL'
+            else:
+                continue
 
             timestamp = int(time.time() * 1000)
 
             if symbol not in result:
-                result[symbol] = {}
-            result[symbol]= {
+                result[symbol] = []
+            result[symbol].append({
                 'coin': symbol,
-                'adx': current_adx,
+                'fastk': current_fastk,
+                'fastd': current_fastd,
+                'signal': signal,
                 'date': timestamp,
                 'interval': interval
-            }
+            })
 
-    cache.set('adx_data', result, timeout=3600)
+    cache.set('stoch_rsi_data', result, timeout=3600)
     return jsonify(result)
 
-@app.route('/adx/<string:interval>')
-def get_adx_interval(interval):
-    data = cache.get('adx_data')
+@app.route('/stoch_rsi/<string:interval>')
+def get_stoch_rsi_interval(interval):
+    data = cache.get('stoch_rsi_data')
     if data is None:
-        return jsonify({'error': 'ADX data not available'})
+        return jsonify({'error': 'Stochastic RSI data not available'})
 
-    result = {symbol: data[symbol] for symbol in data if data[symbol]['interval'] == interval}
-    cache.set('adx_data_interval', result, timeout=3600)
+    result = {symbol: [entry for entry in data[symbol] if entry['interval'] == interval] for symbol in data}
+    cache.set('stoch_rsi_data_interval', result, timeout=3600)
     return jsonify(result)
 
 if __name__ == '__main__':
